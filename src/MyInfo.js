2fadb9e4ee53a2837c149a36ee01892e9ffa3934
asantiagot <josantorres93@gmail.com>
2019-08-13 23:58:10 -0500

    Added content.js to display its content in MyInfo.js

--- src/MyInfo.js
+++ src/MyInfo.js
@@ -1,23 +1,34 @@
 import React from "react";
 // import ReactDOM from "react-dom";
 import {hot} from "react-hot-loader";
+import content from "./content";
 
+function ShortDescription() {
+  return (
+    <div>
+      {`${content.age} years old ${content.position} from ${content.location}. Currently working at ${content.company}.`}
+    </div>
+  );
+}
+
+function PortfolioShowcase() {
+  return (
+    <div>
+      <h2>{content.portfolio.length > 1 ? `This is a list of the projects I've worked on:` : `This is the project I've worked on:`}</h2>
+    </div>
+  )
+}
 function MyInfo() {
-  return(
-      <div>
-        <h1>
-          Antonio
-        </h1>
-          <p>
-            My name is Antonio, I like to play tennis and to build websites.
-          </p>
-          <ol >Top 3 vacation sites:
-            <li>Japan</li>
-            <li>Madrid</li>
-            <li>Paris</li>
-          </ol>
-      </div>
+  return (
+    <div>
+      <h1>
+        Hello! I'm {content.name}
+      </h1>
+      <ShortDescription/>
+      <PortfolioShowcase/>
+    </div>
     );
-  }
+}
 
-  export default hot(module) (MyInfo);
+
+export default hot(module) (MyInfo);
---
+++ src/content.js
@@ -0,0 +1,20 @@
+'use strict'
+
+let content = {
+    name: "Antonio Santiago",
+    position: "Frontend Developer",
+    
+    age: 25,
+    location: "Zapopan, Jalisco",
+    company: "Oracle",
+    portfolio: {
+        myPortfolio: {
+            name: "Generic Portfolio Generator",
+            summary: "A super easy generator that uses a JSON file to create your portfolio",
+            thumbnail: "images/myPortfolio.png",
+        }
+    }
+
+}
+
+export default (content);
