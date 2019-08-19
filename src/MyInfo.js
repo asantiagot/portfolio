013b3d2dfa17bc8e25817dd50a027aa9e93aa527
asantiagot <josantorres93@gmail.com>
2019-08-19 00:04:58 -0500

    Added styling in MyInfo.js

--- src/MyInfo.js
+++ src/MyInfo.js
@@ -2,6 +2,26 @@ import React from "react";
 // import ReactDOM from "react-dom";
 import {hot} from "react-hot-loader";
 import content from "./content";
+import "../styles/style.css";
+
+function NavigationItem(props) {
+  return (
+    <li>{props.itemName}</li>
+  )
+}
+
+function NavigationBar() {
+  return (
+    <nav id="site-nav" role="navigation">
+      <ul>
+        <NavigationItem itemName={"HOME"}/>
+        <NavigationItem itemName={"PROJECTS"}/>
+        <NavigationItem itemName={"ABOUT"}/>
+        <NavigationItem itemName={"TRAJECTORY"}/>
+      </ul>
+    </nav>
+  )
+}
 
 function ShortDescription() {
   return (
@@ -11,7 +31,7 @@ function ShortDescription() {
   );
 }
 
-function PortfolioShowcase() {
+function PortfolioShowcase() { 
   return (
     <div>
       <h2>{content.portfolio.length > 1 ? `This is a list of the projects I've worked on:` : `This is the project I've worked on:`}</h2>
@@ -21,6 +41,7 @@ function PortfolioShowcase() {
 function MyInfo() {
   return (
     <div>
+      <NavigationBar/>
       <h1>
         Hello! I'm {content.name}
       </h1>
