14529bf10ab9ffd4eb767b20b1bfdb38c438f761
asantiagot <josantorres93@gmail.com>
2019-08-28 23:57:48 -0500

    added personalized webpack config files, one for development and one for production

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+
+  plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+          title:'Production'
+      })
+  ]
+
+};
