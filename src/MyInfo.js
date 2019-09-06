fbe49bce78adbe80c6b0ead09cc95039837c7230
asantiagot <josantorres93@gmail.com>
2019-09-05 23:03:15 -0500

    added Trajectory section to page, as well as CSS

--- src/MyInfo.js
+++ src/MyInfo.js
@@ -30,7 +30,6 @@ function NavigationBar(props) {
 
 function Account(props) {
   const icon = `fab fa-${props.name}`;
-  console.log(icon);
 
   return (
     <a href={props.url}>
@@ -60,9 +59,9 @@ function ContactDescription(props) {
     <section className="description">
       <h1>{props.contact.name}</h1>
       <h2>{props.contact.currentRole}</h2>
-      <h3>
+      {/* <h3>
         {`${props.contact.age} years old ${props.contact.currentRole} from ${props.contact.location}. Currently working at ${props.contact.company}.`}
-      </h3>
+      </h3> */}
       <ContactAccounts accounts={props.accounts}/>
     </section>
   );
@@ -87,10 +86,49 @@ function Contact(props) {
 
 function PortfolioShowcase() { 
   return (
-    <section id="#projects">
+    <section className="projects">
       {/* <h2>{content.projects.length > 1 ? `This is a list of the projects I've worked on:` : `This is the project I've worked on:`}</h2> */}
       <h2>{`A cool site in progress! :)`}</h2>
     </section>
+  );
+}
+
+function TrajectoryItem(props) {
+  const options = {dateStyle: 'short', weekday: 'long', year: 'numeric', month: 'long'};
+  const start = `${props.startDate.toLocaleDateString("en-US", options)}`;
+  const end = props.endDate ? ` - ${props.endDate.toLocaleDateString("en-US", options)}` : ' - today';
+  const date = start + end;
+  return(
+    <section className="trajectoryItem">
+      <div className="trajectoryHeading">
+        <h3 className="company">{props.company}</h3>
+        <h3>{`(${date})`}</h3>
+      </div>
+      <h4 className="role">{props.role}</h4>
+      <p>{props.description}</p>
+    </section>
+  );
+}
+
+function Trajectory(props) {
+  const exp = Object.keys(props.trajectory);
+  exp.reverse();
+  return(
+    <section className="trajectory">
+      <h2>Trajectory</h2>
+      {exp.map((value, index) => {
+        return (
+          <TrajectoryItem exp={value} 
+                          key={index} 
+                          company={props.trajectory[value].company}
+                          role={props.trajectory[value].role}
+                          description={props.trajectory[value].description}
+                          startDate={props.trajectory[value].date.start}
+                          endDate={props.trajectory[value].date.end}
+                          />
+        );
+      })}
+  </section>
   );
 }
 
@@ -98,7 +136,8 @@ function Main(props) {
   return(
     <main>
       <Contact contact={props.content.contact} accounts={props.content.accounts}/>
-      <PortfolioShowcase/>
+      {/* <PortfolioShowcase/> */}
+      <Trajectory trajectory={props.content.trajectory}/>
     </main>
   );
 }
